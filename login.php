ac8f5056b181c8a519e751759f3f9b34c260ed26
rajuptvs <raju.ptvs@gmail.com>
2022-03-11 00:02:43 -0800

    login v6

--- login.php
+++ login.php
@@ -8,12 +8,15 @@ $userinfo = array(
 
 if(isset($_GET['logout'])) {
     $_SESSION['username'] = '';
-    header('Location:  ' . $_SERVER['PHP_SELF']);
+    header('Location:index.php' . $_SERVER['PHP_SELF']);
 }
 
 if(isset($_POST['username'])) {
-    if($userinfo[$_POST['username']] == $_POST['password']) {
-        $_SESSION['username'] = $_POST['username'];
+	
+		$username = $_POST['username']; $password = $_POST['password'];
+		#$userinfo[$_POST['username']] == $_POST['password']
+    if($username === 'admin' && $password === 'password') {
+        #$_SESSION['username'] = $_POST['username'];
         header("Location:contactus.php");
     }else {
         
@@ -31,13 +34,12 @@ if(isset($_POST['username'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
-    <link href="../assests/scripts/aos.css" rel="stylesheet">
-    <link href="../assests/scripts/bootstrap.min.css" rel="stylesheet">
-    <link href="../assests/scripts/bootstrap-icons.css" rel="stylesheet">
+	<link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+
     <!--<link href="../assests/scripts/glightbox.min.css" rel="stylesheet"> -->
     <link href="../assests/scripts/boxicons.min.css" rel="stylesheet">
     <!--<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/> -->
-    <title>GrammerSchool</title>
+    <title>Login</title>
 </head>
         <title>Login</title>
     </head>
@@ -76,31 +78,56 @@ if(isset($_POST['username'])) {
         width: 30%;
         border-radius: 5px;
         height: 40px;
-        background:#E79A88;
-        color: whitesmoke;
+        background:#131113;
+        color: white;
         font-size: 20px;
-        font-family: var(--Sniglet-font);
+      
     }
 </style>
-    <body>
-    <header>
-        <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg ">
-            <img src="https://img.icons8.com/carbon-copy/50/000000/school.png"/>
-            <a class="navbar-brand" href="#">Grammar School</a>
-            
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link scrollto active" href="../Index.php"> Home</a>
-                    </li>  
-                </ul>
-            </nav>
-        </div>
+    <body style="background-color: black">
+   
+        
+         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
        
-    </header>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+             <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+				 
+             </li>
+			  
+			  <li class="nav-item active">
+                <a class="nav-link" href="about.html">About Us&nbsp;<br>
+                </a>
+				 
+             </li>
+			  <li class="nav-item active">
+                <a class="nav-link" href="products.html">Services&nbsp; <span class="sr-only">(current)</span></a>
+				 
+             </li>
+             <li class="nav-item active">
+                <a class="nav-link" href="login.php">Contact Us</a>
+             </li>
+             <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                More
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                   <a class="dropdown-item" href="gallery.html">Gallery</a>
+                   <a class="dropdown-item" href="news.html">News</a>
+                   <div class="dropdown-divider"></div>
+                   <a class="dropdown-item" href="#">Social Media</a>
+                </div>
+             </li>
+          </ul>
+       </div>
+    </nav>
+		
+       
+  
        
         <div style=" display: flex; justify-content: center; align-items: center; height: 100vh; padding: 20px; width: 100%; flex-direction: column;">
         <form method="post" class="loginForm">
@@ -111,5 +138,12 @@ if(isset($_POST['username'])) {
             <input type="submit" id = "submit" name="login" value="login" />
         </form>
         </div>
+		
+		    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+    <script src="js/jquery-3.4.1.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed --> 
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap-4.4.1.js"></script>
     </body>
 </html>
